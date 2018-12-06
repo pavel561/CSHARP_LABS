d5c853aac41c92580e0b16b7caf5e3a661c6924a
PavelGornostaev <pavel561@gmail.com>
2018-12-06 16:46:28 +0300

    Добавьте файлы проекта.

---
+++ CSHARP_LAB1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_LAB1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
