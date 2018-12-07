a7f985b5e855f7979ad224aeaf86957fc3ade506
PavelGornostaev <pavel561@gmail.com>
2018-12-07 14:37:33 +0300

    Приложение создано

--- CSHARP_LAB1/Program.cs
+++ CSHARP_LAB1/Program.cs
@@ -8,8 +8,27 @@ namespace CSHARP_LAB1
 {
 	class Program
 	{
+
 		static void Main(string[] args)
 		{
-		}
+			string UserName;
+			string UserSurname;
+			int UserAge;
+			string[] Documents = new string[]
+                {
+					"Application",
+					"Permission"
+				};
+            Console.Write("Введите ваше имя >> ");
+            UserName = Console.ReadLine();
+            Console.Write("Введите вашу Фамилию >> ");
+            UserSurname = Console.ReadLine();
+			Console.Write("Введите ваш возраст >> ");
+			Int32.TryParse(Console.ReadLine(), out UserAge);
+			Console.WriteLine($"Привет, {UserName} {UserSurname}");
+			Console.WriteLine($"Ваш возраст >> {UserAge}");
+
+			Console.ReadKey();
+        }
 	}
 }
